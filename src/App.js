35672e0712d8f0a9109dc0b16c36113e9aa9d6be
Gustavo Kozonoe Maricato Rodrigues <gustavo@mpr.com.br>
2021-10-01 10:03:22 -0300

    Add Input Handlers to Menu
    Co-Authored-By: Patrick Parizotti <70484961+PatrickRizers@users.noreply.github.com>
    Co-Authored-By: Vitor Grechi Kuninari <vitorgk1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import './App.css';
-import './styles/fonts.css'
+import React, { useState } from "react";
+
+import "./App.css";
+import "./styles/fonts.css";
 
 function App() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [count, setCount] = useState(0);
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
   
+  function handleInputBlur() {
+    setIsFocused(false);
+  }
+
+  function handleInputChange(e) {
+    var input = document.getElementById('key').value.length;
+    setCount(input);
+  }
+
+  function handleKeyDown (event) {
+    const badCharacter = ' '
+    if(event.nativeEvent.key === badCharacter){
+      event.preventDefault();
+    }
+  }
+
   return (
-    <div className="App">
-        <h1>Catapimbas</h1>
-      <div className="buttons">
-        <button className="createRoom">Criar Sala</button>
-        <div className="enterRoom">
-          <form>
-            <input type="text" id="key" name="key" placeholder="Coloque o código" />
-          </form>
-          <button>(☞ﾟヮﾟ)☞</button>
-        </div>
+  <div className="App">
+    <h1>Catapimbas</h1>
+    <div className="buttons">
+      <button className="createRoom">Criar Sala</button>
+      <div className="enterRoom">
+        <form>
+          <input
+            type="text"
+            id="key"
+            name="key"
+            autoComplete="off"
+            maxLength="5"
+            pattern="[a-zA-Z0-9]+"
+            placeholder="Coloque o código"
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            onInputCapture={handleInputChange}
+            onKeyDownCapture={handleKeyDown}
+            style = {isFocused ? {backgroundColor: "#83bfe2"} : {}}
+          />
+        </form>
+        <button>
+          {count === 5 ? "➜" : ""}
+        </button>
       </div>
     </div>
+  </div>
   );
 }
 
