9412cf3fb77bb19f9f5ff5996da2db1e3f07a84d
VitorGK <vitorgk1@gmail.com>
2021-11-05 10:13:08 -0300

    Add remove lifes logic

--- server/logica/Player.py
+++ server/logica/Player.py
@@ -5,12 +5,13 @@ from Card import Card
 class Player:
     def __init__(self, lifes: int) -> None:
         self.lifes: int = lifes
-        self.points: int = 0
+        self.current_points: int = 0
+        self.target_points: int = 0
         self.cards: List[Card] = []
 
     def add_point(self):
-        self.points += 1
+        self.current_points += 1
 
     def update_lifes(self):
-        self.lifes -= abs(self.points)
+        self.lifes -= abs(self.target_points - self.current_points)
         self.points = 0
